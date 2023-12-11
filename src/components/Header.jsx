b3a65c28be32215ddf4b34ad404621fcb49e92b5
Jose Munoz cardozo <josemunozcardozo@MacBook-Air-de-Jose.local>
2023-12-11 16:18:26 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+        ¿Cuanto <span className="text-indigo-600">dinero</span> necesitas?
+      </h1>
+    </>
+  );
+}
+
+export default Header;
